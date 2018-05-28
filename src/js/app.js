068c3b911807a4b4674624d3258435f22deb70ae
Andrew Smith <andrew.smith03@adelaide.edu.au>
2018-05-28 14:28:43 +0930

    remove all the unused styles and markup and files
    Signed-off-by: Andrew Smith <andrew.smith03@adelaide.edu.au>

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
-// Import the header footer
-import './header-footer'
+import 'foundation-sites/js/entries/foundation';
+import 'what-input';
+// import '../../node_modules/motion-ui/dist/motion-ui.js';
+import '../../src/assets/js/vendor/modernizr.js';
 
 // app.js only imports and code goes here
 import './plugins';
+
+// OffCanvas global settings
+Foundation.OffCanvas.defaults.transitionTime = '.2s';
+Foundation.OffCanvas.defaults.transition = 'overlap';
+
