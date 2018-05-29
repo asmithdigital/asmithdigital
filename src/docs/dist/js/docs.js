4c14d00707834538b00e8f153f276672e7f07c2f
Andrew Smith <andrew.smith03@adelaide.edu.au>
2018-05-29 15:09:50 +0930

    finalise portfolio styles
    Signed-off-by: Andrew Smith <andrew.smith03@adelaide.edu.au>

--- src/docs/dist/js/docs.js
+++ src/docs/dist/js/docs.js
@@ -1526,77 +1526,148 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 (function ($, window, document, undefined) {
-    'use strict';
+  'use strict';
 
-    // init cubeportfolio
+  // init cubeportfolio
 
-    $('#js-grid-full-width').cubeportfolio({
-        filters: '#js-filters-full-width',
-        layoutMode: 'mosaic',
-        sortByDimension: true,
-        defaultFilter: '*',
-        animationType: 'fadeOutTop',
-        gapHorizontal: 0,
-        gapVertical: 0,
-        gridAdjustment: 'responsive',
-        mediaQueries: [{
-            width: 1500,
-            cols: 5
-        }, {
-            width: 1100,
-            cols: 4
-        }, {
-            width: 800,
-            cols: 3
-        }, {
-            width: 480,
-            cols: 2,
-            options: {
-                caption: '',
-                gapHorizontal: 10,
-                gapVertical: 10
-            }
-        }],
-        caption: 'zoom',
-        displayType: 'fadeIn',
-        displayTypeSpeed: 100,
+  $('#js-grid-lightbox-gallery').cubeportfolio({
+    filters: '#js-filters-juicy-projects',
+    layoutMode: 'grid',
+    mediaQueries: [{
+      width: 1500,
+      cols: 4
+    }, {
+      width: 1100,
+      cols: 3
+    }, {
+      width: 800,
+      cols: 3
+    }, {
+      width: 480,
+      cols: 2,
+      options: {
+        caption: ''
+      }
+    }],
+    defaultFilter: '*',
+    animationType: 'quicksand',
+    gapHorizontal: 0,
+    gapVertical: 0,
+    gridAdjustment: 'responsive',
+    caption: 'overlayBottomPush',
+    displayType: 'sequentially',
+    displayTypeSpeed: 100,
 
-        // lightbox
-        lightboxDelegate: '.cbp-lightbox',
-        lightboxGallery: true,
-        lightboxTitleSrc: 'data-title',
-        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+    // lightbox
+    lightboxDelegate: '.cbp-lightbox',
+    lightboxGallery: true,
+    lightboxTitleSrc: 'data-title',
+    lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
 
-        // singlePage popup
-        singlePageDelegate: '.cbp-singlePage',
-        singlePageDeeplinking: true,
-        singlePageStickyNavigation: true,
-        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
-        singlePageCallback: function singlePageCallback(url, element) {
-            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-            var t = this;
+    // singlePageInline
+    singlePageInlineDelegate: '.cbp-singlePageInline',
+    singlePageInlinePosition: 'below',
+    singlePageInlineInFocus: true,
+    singlePageInlineCallback: function singlePageInlineCallback(url, element) {
+      // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+      var t = this;
 
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'html',
-                timeout: 30000
-            }).done(function (result) {
-                t.updateSinglePage(result);
-            }).fail(function () {
-                t.updateSinglePage('AJAX Error! Please refresh the page!');
-            });
-        },
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+        timeout: 30000
+      }).done(function (result) {
 
-        plugins: {
-            loadMore: {
-                element: '#js-loadMore-full-width',
-                action: 'auto',
-                loadItems: 3
-            }
-        }
-    });
+        t.updateSinglePageInline(result);
+      }).fail(function () {
+        t.updateSinglePageInline('AJAX Error! Please refresh the page!');
+      });
+    },
+
+    plugins: {
+      loadMore: {
+        element: '#js-loadMore-lightbox-gallery',
+        action: 'click',
+        loadItems: 3
+      }
+    }
+  });
 })(jQuery, window, document);
+
+// (function($, window, document, undefined) {
+//     'use strict';
+//
+//     // init cubeportfolio
+//     $('#js-grid-full-width').cubeportfolio({
+//         filters: '#js-filters-full-width',
+//         layoutMode: 'mosaic',
+//         sortByDimension: true,
+//         defaultFilter: '*',
+//         animationType: 'fadeOutTop',
+//         gapHorizontal: 0,
+//         gapVertical: 0,
+//         gridAdjustment: 'responsive',
+//         mediaQueries: [{
+//             width: 1500,
+//             cols: 5,
+//         }, {
+//             width: 1100,
+//             cols: 4,
+//         }, {
+//             width: 800,
+//             cols: 3,
+//         }, {
+//             width: 480,
+//             cols: 2,
+//             options: {
+//                 caption: '',
+//                 gapHorizontal: 10,
+//                 gapVertical: 10,
+//             }
+//         }],
+//         caption: 'zoom',
+//         displayType: 'fadeIn',
+//         displayTypeSpeed: 100,
+//
+//         // lightbox
+//         lightboxDelegate: '.cbp-lightbox',
+//         lightboxGallery: true,
+//         lightboxTitleSrc: 'data-title',
+//         lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+//
+//         // singlePage popup
+//         singlePageDelegate: '.cbp-singlePage',
+//         singlePageDeeplinking: true,
+//         singlePageStickyNavigation: true,
+//         singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+//         singlePageCallback: function(url, element) {
+//         // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+//         var t = this;
+//
+//         $.ajax({
+//           url: url,
+//           type: 'GET',
+//           dataType: 'html',
+//           timeout: 30000
+//         })
+//           .done(function(result) {
+//             t.updateSinglePage(result);
+//           })
+//           .fail(function() {
+//             t.updateSinglePage('AJAX Error! Please refresh the page!');
+//           });
+//         },
+//
+//         plugins: {
+//             loadMore: {
+//                 element: '#js-loadMore-full-width',
+//                 action: 'auto',
+//                 loadItems: 3,
+//             }
+//         },
+//     });
+// })(jQuery, window, document);
 
 /***/ }),
 /* 7 */
