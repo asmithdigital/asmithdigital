6242850c2d97b7d14f05bf0231030a336a71a8c3
Andrew Smith <andrew.smith03@adelaide.edu.au>
2018-06-20 16:00:04 +0930

    auto refresh the hero image, add comments
    Signed-off-by: Andrew Smith <andrew.smith03@adelaide.edu.au>

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,7 @@ import './plugins';
 // Temporarily use foundation
 $(document).foundation();
 
+// Scroll class toggle
 $(document).ready(function() {
   $(window).scroll(function() {
     var scrollTop = $(window).scrollTop();
@@ -19,3 +20,14 @@ $(document).ready(function() {
     }
   });
 });
+
+
+// Auto change the hero image
+$(document).ready(function() {
+  setInterval(function () {
+    var $element = $('.hero');
+    $element.css('background-image', 'url("https://picsum.photos/1405/1053?random&time=' + (+new Date()) + '")');
+  }, 1000 * 15);
+});
+
+
