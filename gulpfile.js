cbd38f9dd3b7cf4aff15148f026fce860d038363
Andrew Smith <andrewsmith@andrews-air.home>
2021-10-22 13:52:31 +1030

    adding packages and scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,41 @@
 const { src, dest, series, parallel, watch } = require('gulp');
+var sass = require('gulp-sass');
+var $             = require('gulp-load-plugins')();
+var autoprefixer  = require('autoprefixer');
+
+var sassPaths = [
+  'node_modules/foundation-sites/scss',
+  'node_modules/motion-ui/src'
+];
 
 function clean(cb) {
   // body omitted
   cb();
 }
 
-function css(cb) {
-  // body omitted
-  cb();
-}
+// function css(cb) {
+//   return src('./scss/app.scss')
+//   .pipe(sass())
+//   .pipe(dest('./css'))
+// }
 
-function javascript() {
-    return src('*.js')
-    .pipe(dest('output'));
+function css() {
+  return src('./scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    // .pipe($.postcss([
+    //   autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
+    // ]))
+    .pipe(dest('./css'));
+};
+
+function javascript(cb) {
+    // return src('*.js')
+    // .pipe(dest('output'));
+    cb();
 }
 
 exports.default = function() {
@@ -23,4 +46,5 @@ exports.default = function() {
 };
 
 exports.build = series(clean, parallel(css, javascript));
+exports.css = series(css);
 
