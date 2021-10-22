21eebeeead31304db7045bdb99fcbca282c183da
Andrew Smith <andrewsmith@andrews-air.home>
2021-10-22 15:17:48 +1030

    adding assets, gulp tasks, and fixing build errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,11 @@
 const { src, dest, series, parallel, watch } = require('gulp');
 var sass = require('gulp-sass');
 var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
-
-function clean(cb) {
-  // body omitted
-  cb();
-}
-
-// function css(cb) {
-//   return src('./scss/app.scss')
-//   .pipe(sass())
-//   .pipe(dest('./css'))
-// }
 
 function css() {
   return src('./scss/app.scss')
@@ -26,25 +14,28 @@ function css() {
       outputStyle: 'compressed' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
-    // .pipe($.postcss([
-    //   autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
-    // ]))
     .pipe(dest('./css'));
 };
 
-function javascript(cb) {
-    // return src('*.js')
-    // .pipe(dest('output'));
-    cb();
+function javascript() {
+    return src([
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/foundation-sites/dist/js/foundation.min.js',
+        './js/jquery.cubeportfolio.min.js',
+        './js/main.js',
+        './js/app.js'
+      ])
+    .pipe($.concat('app.jquery.js'))
+    // .pipe($.uglify()
+    //     .on('error', e => {
+    //       console.log(e);
+    //     })
+    // )
+    .pipe(dest('./js'));
 }
 
 exports.default = function() {
-    // You can use a single task
     watch('src/*.css', { ignoreInitial: false, queue: false }, css);
-    // Or a composed task
-    watch('src/*.js', { ignoreInitial: false, queue: false }, series(clean, javascript));
+    watch('src/*.js', { ignoreInitial: false, queue: false }, javascript);
 };
-
-exports.build = series(clean, parallel(css, javascript));
-exports.css = series(css);
-
+exports.build = parallel(css, javascript);
---
+++ js/app.js
@@ -0,0 +1,22 @@
+// import 'foundation-sites/js/entries/foundation';
+// import 'what-input';
+// // import '../../node_modules/motion-ui/dist/motion-ui.js';
+// import '../../src/assets/js/vendor/modernizr.js';
+
+// // app.js only imports and code goes here
+// import './plugins';
+
+// Temporarily use foundation
+$(document).foundation();
+
+// Scroll class toggle
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     var scrollTop = $(window).scrollTop();
+//     if (scrollTop > 1) {
+//       $("#sp-header").addClass("menu-fixed");
+//     } else {
+//       $("#sp-header").removeClass("menu-fixed");
+//     }
+//   });
+// });
